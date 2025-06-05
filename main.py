ad642f63baff3e52453bd8731195861d61ced336
Gregory Hyman <ghyman@remmac06548.local>
2025-06-05 10:02:14 -0400

    WIP pdf writer; adds annotation with expected content to page; page location isn't right

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from typing import List, Union
 from image_analyzer import PDFPageAnalyzer, PDFPageAnalysis, generate_page_analyses_from_file
 from helpers import get_data_uri_for_image
 from pdf_reader import pdf_to_images
+from pdf_writer import write_page_analyses_to_pdf
 
 
 @click.group()
@@ -42,9 +43,10 @@ def start(pdf_path, existing_page_analyses_json=None):
         if not existing_page_analyses_json.is_file() or existing_page_analyses_json.suffix.lower() != '.json':
             raise ValueError('`--use-existing-page-analyses` option requires valid path to a JSON file.')  
 
-    output_path = pdf_path.parent
+    output_dir = pdf_path.parent
+    output_pdf_path = output_dir / f"{pdf_path.stem}_{int(time.time())}.pdf"
 
-
+    annotation_inches_from_left = 6.0 # TODO make configurable
     text_width_inches: float = 4.8 # TODO: make configurable (via menu?)
     should_suggest_resolution = True # TODO: make configurable
 
@@ -63,9 +65,9 @@ def start(pdf_path, existing_page_analyses_json=None):
             click.echo(f"Unable to remove temporary images at {temp_dir}")
         raise e
 
-    click.echo(f"Sending {len(page_images)} pages to AI service for analysis...")
-
     if not existing_page_analyses_json:
+        click.echo(f"Sending {len(page_images)} pages to AI service for analysis...")
+
         page_analyses: Union[List[PDFPageAnalysis], List] = []
 
         # process page_images
@@ -92,14 +94,28 @@ def start(pdf_path, existing_page_analyses_json=None):
         if page_analyses:
             # write analyses to file in case user wants to reload in future
             # e.g., user needs a new annotated PDF and doesn't want to rerun analysis service
-            analyses_save_filepath = output_path / f"page_analyses_backup_{int(time.time())}.json"
+            analyses_save_filepath = output_dir / f"page_analyses_backup_{int(time.time())}.json"
             with open(str(analyses_save_filepath), 'w') as f:
                 json.dump([pa.model_dump() for pa in page_analyses], f)
             click.echo(f"Raw page analyses (JSON format) backed up at {analyses_save_filepath}")
     else:
         page_analyses: List[PDFPageAnalysis] = generate_page_analyses_from_file(existing_page_analyses_json)
     
+    # generate annotated PDF representation and write to file
+    annotated_pdf = write_page_analyses_to_pdf(
+		page_analyses=page_analyses, 
+		original_pdf_filepath=pdf_path,
+		annotate_failed_analyses=False,
+		inches_from_left=annotation_inches_from_left,
+		should_suggest_resolution=should_suggest_resolution
+	)
+    annotated_pdf.save(output_pdf_path)
+    annotated_pdf.close()
+    click.echo(f"Annotated PDF written to {output_pdf_path}")
+
+
     # TODO: write back to copy of PDF or, optionally, txt (or csv?)
+    # TODO: option to include annotation for failed page analyses in output PDF
 
     # clean up temp_dir recursively at the end
     shutil.rmtree(temp_dir)
---
+++ pdf_writer.py
@@ -0,0 +1,111 @@
+import fitz
+import logging
+from pathlib import Path
+from typing import List, Union
+
+from image_analyzer import PDFPageAnalysis
+
+
+logger = logging.Logger
+
+
+def write_page_analyses_to_pdf(
+		page_analyses: List[PDFPageAnalysis], 
+		original_pdf_filepath: Union[str, Path],
+		annotate_failed_analyses: bool = False,
+		inches_from_left: float = None, # inches from left of pdf page for locating annotation
+		should_suggest_resolution: bool = False
+	):
+	"""
+	Create a copy of input PDF with annotations
+	at the locations of found issues.
+
+	Returns a pymupdf representation of annotated doc.
+	"""
+	doc = fitz.open(original_pdf_filepath)
+
+	for page_num in range(doc.page_count):
+		page = doc[page_num]
+
+		# get first matching page_analysis (there should be 1 or none)
+		page_analysis = next(iter([pa for pa in page_analyses if pa.page_num == page_num]), None)
+
+		if not page_analysis:
+			continue
+
+		if page_analysis.analysis_failed:
+			if not annotate_failed_analyses:
+				logger.warning(f"Analysis failed on PDF p. {page_num + 1} (absolute doc page number).")
+			else:
+				add_text_annotation_to_page(
+					page=page,
+					annotation_text="PROD: Page analysis failed.",
+				)
+			continue
+		elif not page_analysis.page_has_issue:
+			logger.info(f"No issues found on PDF p. {page_num + 1} (absolute doc page number).")
+			continue
+		
+		for page_issue in page_analysis.page_problematic_code_blocks:
+			inches_from_top = page_issue.inches_from_top or None
+		
+			annotation_text = f"Code running into margin.\n\n{'AI-generated reformatting suggestion:\n\n' + page_issue.reformatting_suggestion if should_suggest_resolution and page_issue.reformatting_suggestion else ''}"
+
+			add_text_annotation_to_page(
+				page=page,
+				annotation_text=annotation_text,
+				inches_from_left=inches_from_left,
+				inches_from_top=inches_from_top
+			)
+	
+	return doc
+
+		
+def add_text_annotation_to_page(
+	page, # pymupdf representation of pdf page
+	annotation_text: str,
+	inches_from_left: Union[float, None] = None,
+	inches_from_top: Union[float, None] = None
+):
+	try:
+		page_rect = page.rect
+	except Exception as e:
+		logger.error(f"Unable to obtain rect from page. {e}")
+		return None
+
+	if not annotation_text:
+		return None
+
+	page_width = page_rect.width
+
+	if inches_from_left/72 > page_width:
+		logger.error(f"Invalid value for `inches_from_left`: {inches_from_left}. Must be less than {page_width*72}. Falling back to default.")
+		inches_from_left = None
+
+	if not inches_from_top:
+		if not inches_from_left:
+			point = (page_width - 72, 72) # 1 inch from right, 1 inch from top
+		else:
+			point = (inches_from_left/72, 72)
+	else:
+		if not inches_from_left:
+			inches_from_left = (page_width - 72)/72 # fallback/default inches from left
+		try:
+			point = (inches_from_left/72, inches_from_top/72) # point in inches to points
+		except Exception as e:
+			logger.error(f"Unable to calculate location for annotation on p. {page.page_num + 1} (absolute page num). {e}")
+			return None
+	
+	annot = page.add_text_annot(
+		point,
+		annotation_text
+	)
+	annot.set_info(title="PDF Analysis Script")
+	annot.update()
+
+
+def write_page_analyses_to_text():
+	"""
+	Write page analyses to text.
+	"""
+	pass
